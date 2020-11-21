18e48aa49843c46f90dcccb58655cc52d35a93e2
Ethan4810 <jsnam3379@gmail.com>
2020-11-21 13:45:49 +0900

    mac branch update by JS

---
+++ main.py
@@ -0,0 +1,9 @@
+# print name func
+
+# main
+def print_hi(name):
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+# input name
+if __name__ == '__main__':
+    print_hi('Minjae and Jongsu')
